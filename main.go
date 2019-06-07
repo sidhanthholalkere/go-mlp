dc9bfa324cb767c832c01a1de86fdbd059c324bc
Sidhanth Holalkere <44298843+sidhanthholalkere@users.noreply.github.com>
2019-06-07 15:54:04 -0400

    Update main.go

--- main.go
+++ main.go
@@ -9,12 +9,12 @@ import (
 func main() {
 	rand.Seed(1)
 
-	Avery := nn.NewNN([]int{3, 2}, 2)
+	mlp := nn.NewNN([]int{3, 2}, 2)
 	inputs := []float64{1.5, 2.5}
 	outputs := []float64{1, 0}
-	Avery.Propogate(inputs)
-	Avery.Backpropogate(outputs, inputs)
-	Avery.Print()
-	cost := Avery.GetCost(outputs)
+	mlp.Propogate(inputs)
+	mlp.Backpropogate(outputs, inputs)
+	mlp.Print()
+	cost := mlp.GetCost(outputs)
 	fmt.Println("Cost: ", cost)
 }
